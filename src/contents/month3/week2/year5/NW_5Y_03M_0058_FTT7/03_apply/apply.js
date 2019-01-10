b38a46dd659b123a6af60e8835493a6a886fb72c
kyung-doo <hohomkd@naver.com>
2019-01-10 15:57:16 +0900

    fgfg

--- src/contents/month3/week2/year5/NW_5Y_03M_0058_FTT7/03_apply/apply.js
+++ src/contents/month3/week2/year5/NW_5Y_03M_0058_FTT7/03_apply/apply.js
@@ -1,5 +1,12 @@
 $(function ()
 {
-	
+    $(".big-btn").on("button.click", function ( e )
+    {
+        $(".hand.normal").hide();
+    });
 
+    $(".small-btn").on("button.click", function ( e )
+    {
+        $(".hand.ani").hide();
+    });
 });
